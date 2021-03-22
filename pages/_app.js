78d8b1b19a8fa8b6c39c8f3fc51263ea767b4c33
Zubair Hasan Zubo <zubairhasan@staffasia.org>
2021-03-22 17:20:18 +0600

    Swipper not found removed

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,11 @@
-
-import Layout from '../components/Layout'
-import 'swiper/swiper.scss';
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
